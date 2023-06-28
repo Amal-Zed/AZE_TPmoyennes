4272cf91c580207ecdb8797e3bc2c55b9d99f618
Amal ZED <zedda.amal@gmail.com>
2023-06-28 15:44:35 +0200

    Ajoutez des fichiers projet.

---
+++ Classe.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AZE_TPmoyennes
+{
+    class Classe
+    {
+        public string nomClasse { get; private set; }
+        public List<Eleve> eleves { get; private set; }
+        public List<string> matieres { get; private set; }
+
+        public Classe(string NomClasse)
+        {
+            nomClasse = NomClasse;
+            eleves = new List<Eleve>();
+            matieres = new List<string>();
+        }
+
+        public void ajouterEleve(string prenom, string nom)
+        {
+            Eleve eleveclass = new Eleve(prenom, nom);
+            eleves.Add(eleveclass);
+        }
+
+        public void ajouterMatiere(string matiere)
+        {
+            matieres.Add(matiere);
+        }
+
+        // La moyenne de la classe dans une matière
+        public double Moyenne(int matiere)
+        {
+            double moymatclass = 0;
+            for (int i = 0; i < eleves.Count; i++)
+            {
+                    double moymateleve = eleves[i].Moyenne(matiere);
+                    moymatclass = moymatclass + moymateleve;
+            }
+
+            moymatclass = Math.Truncate((moymatclass / eleves.Count) * 100) / 100;
+            return moymatclass ;
+        }
+
+        // La moyenne générale de la classe
+        public double Moyenne()
+        {
+            double moygeneclass = 0;
+            for (int j = 0; j < matieres.Count; j++)
+            {
+                double moymatgene = Moyenne(j);
+                moygeneclass = moygeneclass + moymatgene;
+            }
+
+            moygeneclass = Math.Truncate((moygeneclass / matieres.Count) * 100) / 100;
+            return moygeneclass;
+        }
+
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AZE_TPmoyennes
+{
+    class Eleve
+    {
+        public string nom { get; private set; }
+        public string prenom { get; private set; }
+        public List<Note> notes { get; private set; }
+
+        public Eleve(string NomEleve, string PrenomEleve)
+        {
+            nom = NomEleve;
+            prenom = PrenomEleve;
+            notes = new List<Note>();
+        }
+
+        public void ajouterNote(Note note)
+        {
+            notes.Add(note);
+        }
+
+        // La moyenne d'un élève dans une matière
+        public double Moyenne(int matiere)
+        {
+            double moymatiere = 0;
+            
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (notes[i].matiere == matiere)
+                {
+                    moymatiere = moymatiere + notes[i].note;
+                }
+            }
+
+            moymatiere = Math.Truncate((moymatiere / 5) * 100) / 100;
+            return moymatiere ;
+        }
+
+        // La moyenne générale du même élève
+        public double Moyenne()
+        {
+            double moygenerale = 0;
+            int NumMat = 0;
+            double MoyMati = Moyenne(NumMat);
+
+            while (MoyMati != 0) 
+            {
+                MoyMati = Moyenne(NumMat);
+                moygenerale = moygenerale + MoyMati;
+                NumMat++;
+            } 
+
+            moygenerale = Math.Truncate((moygenerale / NumMat) * 100) / 100;
+            return moygenerale;
+  
+        }
+
+    }
+}
